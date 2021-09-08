abb9eea93c5f233d0ac6e31c98d46d6349ec1fbe
koopolina <koopolina@gmail.com>
2021-09-08 21:52:13 +0300

    Внесла правки

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,11 +1,22 @@
 const feedbackForm = document.querySelector(".feedback__form");
-const feedbackPopup = document.querySelector(".modal");
+const failurePopup = document.querySelector(".modal-failure");
+const successPopup = document.querySelector(".modal-success");
+const failureClose = document.querySelector(".modal-failure__button");
+const feedbackName = document.querySelector(".personality-info__input--name");
+const feedbackSurname = document.querySelector(".personality-info__input--surname");
+const feedbackContacts = document.querySelector(".contacts-info__input");
 
 feedbackForm.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  feedbackPopup.classList.add("modal--show");
+  if (!feedbackName.value || !feedbackSurname.value || !feedbackContacts.value) {
+    evt.preventDefault();
+    failurePopup.classList.add("modal-failure--show");
+  } else {
+    successPopup.classList.add("modal-success--show")
+  }
 });
 
-// feedbackButton.addEventListener("click", function (evt) {
-//   feedbackPopup.classList.add("modal-show");
-// });
+failureClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  failurePopup.classList.remove("modal-failure--show");
+  successPopup.classList.remove("modal-success--show");
+});
